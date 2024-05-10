61a1490692715c454f23a5fca27d75bf49d23656
Filipe Pires <filipepmachado1997@gmail.com>
2024-05-10 19:37:52 -0300

    imp#202405101937 - Implementando background e navbar

--- back-end/app.py
+++ back-end/app.py
@@ -25,7 +25,7 @@ safety_settings = {
 
 # Criar um modelo gerativo
 model = genai.GenerativeModel(
-    model_name="gemini-1.0-pro",
+    model_name="gemini-1.0-pro-latest",
     generation_config=generation_config,
     safety_settings=safety_settings
 )
--- front-end/script.js
+++ front-end/script.js
@@ -18,7 +18,7 @@ function sendMessage() {
     .then(response => response.json())
     .then(data => {
         // Exibir resposta do chatbot no chat
-        displayMessage("Med Bot", data.response);
+        displayMessage("Med", data.response);
     })
     .catch(error => {
         console.error("Erro ao enviar mensagem:", error);
@@ -29,8 +29,19 @@ function displayMessage(sender, message) {
     var chatBox = document.getElementById("chat-box");
     var messageElement = document.createElement("div");
     messageElement.innerHTML = `
-        <strong>${sender}:</strong> ${marked(message)}
+        <strong style="color: #5F9EA0;">${sender}:</strong> ${marked(message)}
     `;
     chatBox.appendChild(messageElement);
     chatBox.scrollTop = chatBox.scrollHeight; // Rolar para baixo automaticamente
 }
+
+// Capturar eventos de teclado no textarea
+document.getElementById("user-input").addEventListener("keydown", function(event) {
+    // Verificar se a tecla pressionada é Enter e Shift está pressionado
+    if (event.key === "Enter" && event.shiftKey) {
+        // Adicionar uma quebra de linha
+        this.value += "\n";
+        // Impedir o envio do formulário
+        event.preventDefault();
+    }
+});
